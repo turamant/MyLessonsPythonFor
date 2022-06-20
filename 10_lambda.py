2e6ea8757f0ce2a4cf5bdf7cda3dfd706963a93f
askvart <tur1amant@gmail.com>
2022-06-20 18:08:24 +0300

    lambda

---
+++ 10_lambda.py
@@ -0,0 +1,21 @@
+my_list = [[3, 2], [4, 2], [6, 2], [2, 6], [5, 9], [0, 2]]
+
+print(my_list)
+my_list.sort(key=lambda x: x[0])
+print(my_list)
+my_list.sort(key=lambda x: x[1])
+print(my_list)
+
+
+def func_add(x, y):
+    return x + y
+
+
+if __name__ == '__main__':
+    res = func_add(1, 2)
+    print(res)
+    res2 = lambda x, y: x + y
+    print(res2(1, 2))
+
+    res3 = lambda x, y: x * y
+    print(res3(3, 4))
